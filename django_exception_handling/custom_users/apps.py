8d8ff04c9a4690a7e54aac3339e8ca08a596e474
Konstantin Kostov <kosta.norge@gmail.com>
2022-12-07 11:37:22 +0100

    exception first commit

---
+++ django_exception_handling/custom_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomUsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_exception_handling.custom_users'
